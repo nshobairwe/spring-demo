3c626cdbbd0a2ece0885f302445dad1cc32da44a
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-09-23 21:48:25 +0300

    java object to json object by the help of jackson library

--- src/main/java/com/witnes/Main.java
+++ src/main/java/com/witnes/Main.java
@@ -1,13 +1,13 @@
 package com.witnes;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //@SpringBootApplication
 @ComponentScan
@@ -22,7 +22,26 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greeting(){
-        return "Hello witnes this is my spring boot";
+    public GreetingData greeting(){
+        //return "Hello witnes this is my spring boot";
+        GreetingData response = new GreetingData(
+                "Hellow world",
+                List.of("java","javascript","python"),
+                new Person(
+                        "Innocent kipilipili",
+                        26,
+                        3000)
+                );
+        return response;
     }
+
+    record GreetingData(
+            String greet,
+            List<String> programmingLanguages,
+            Person person){
+    }
+
+    record Person(
+            String person,int age, int savings
+    ){}
 }
