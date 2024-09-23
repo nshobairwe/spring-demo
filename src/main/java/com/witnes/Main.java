4ae78cb7ba3d7034a89c303c499ea171f0479dfd
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-09-23 21:08:11 +0300

    @spring boot annotation replaced by other three annotations

--- src/main/java/com/witnes/Main.java
+++ src/main/java/com/witnes/Main.java
@@ -1,11 +1,18 @@
 package com.witnes;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@Configuration
 @RestController
 public class Main {
     public static void main (String[] args){
